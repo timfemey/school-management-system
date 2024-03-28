95a82061e0bf67ea712db1b98912bec5b9426a00
timfemey <isholaobafemi@gmail.com>
2024-03-28 15:54:53 +0100

    patch: install firebase and setup firebase config

---
+++ client/src/config/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { initializeAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAlE79C6-pd1w-L8LXws6E4wcWH3q4NbEA",
+    authDomain: "school-management-system-4a705.firebaseapp.com",
+    projectId: "school-management-system-4a705",
+    storageBucket: "school-management-system-4a705.appspot.com",
+    messagingSenderId: "172340354704",
+    appId: "1:172340354704:web:df1c15ce90ecb784098215"
+};
+const app = initializeApp(firebaseConfig)
+const auth = initializeAuth(app)
+
+
+export { auth }
