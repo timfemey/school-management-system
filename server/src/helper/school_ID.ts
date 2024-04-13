61d62118a88ea2deecdd6c6d545e5c1c12510457
timfemey <isholaobafemi@gmail.com>
2024-04-13 14:25:15 +0100

    feat: check if school ID has been used function

---
+++ server/src/helper/school_ID.ts
@@ -0,0 +1,13 @@
+import { firestore } from "../config/firebase.js";
+
+export async function checkIfSchoolIdHasBeenUsed(id: string) {
+    try {
+        const doc = await firestore.collection("requests").doc(id).get()
+        const exists = doc.exists
+        return exists;
+    } catch (error) {
+        console.error("Error Occured while checking if School Id has been used")
+        console.error(error)
+    }
+
+}
